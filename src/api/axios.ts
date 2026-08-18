410a52aebf3ab453b4a840a10ac9bb9774b2fac8
chetan <chetansrs34@gmail.com>
2026-08-18 19:44:57 +0530

    fix: updated axios base url for vercel proxy

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -2,7 +2,10 @@
 import axios from 'axios';
 
 // Remove the /api from the base variable here so Sockets can use the root
-const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000';
+// const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000';
+
+
+const BASE_URL = import.meta.env.VITE_API_URL || '';
 
 const API = axios.create({
   baseURL: `${BASE_URL}/api`, // Axios gets the /api suffix here
